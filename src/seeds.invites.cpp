0ac885d64ee5d683772eb7f9801ac497db94e190
chuck <cfharr@gmail.com>
2023-04-03 11:16:07 -0700

    Strip unused files from Seeds (chuck-h/seeds-smart-contracts)

--- src/seeds.invites.cpp
+++
@@ -1,7 +0,0 @@
-#include <seeds.invites.hpp>
-
-void invites::send(name from, name to, asset quantity, string memo) {
-  if (to == get_self()) {
-    check(false, "this contract has been disabled. Please use join.seeds for invitations to SEEDS!");
-  }
-}
